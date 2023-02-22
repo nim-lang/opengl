cc608b9d54587c81380286dd6e42d7ffa96f4e4c
Gabriel Huber <mail@gabrielhuber.at>
2023-02-22 03:51:26 +0100

    Remove GPL copyright notice (Closes #87) (#88)

--- src/opengl/glx.nim
+++ src/opengl/glx.nim
@@ -8,20 +8,6 @@
 #  Version:  3.0
 #  Copyright (C) 1995-1998  Brian Paul
 #
-#  This library is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU Library General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This library is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#  Library General Public License for more details.
-#
-#  You should have received a copy of the GNU Library General Public
-#  License along with this library; if not, write to the Free
-#  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-#
 
 import x11/x, x11/xlib, x11/xutil, opengl
 
